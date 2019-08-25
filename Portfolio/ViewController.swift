2de93d98a47f1222c6840d3174530d90537a927f
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-08-25 16:36:51 +0200

    Initial Commit

---
+++ Portfolio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Portfolio
+//
+//  Created by Andreas Lüdemann on 25/08/2019.
+//  Copyright © 2019 Andreas Lüdemann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
