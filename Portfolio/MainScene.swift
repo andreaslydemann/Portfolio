623616c4cd43133cb053172cbe6c2236dbeb115f
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-08-25 17:25:50 +0200

    Add MainViewController and MainScene

---
+++ Portfolio/MainScene.swift
@@ -0,0 +1,107 @@
+//
+//  MainScene.swift
+//  Portfolio
+//
+//  Created by Andreas Lüdemann on 25/08/2019.
+//  Copyright © 2019 Andreas Lüdemann. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class MainScene: SCNScene {
+    
+    var aboutMeNode :SCNNode = SCNNode()
+    var projectsNode :SCNNode = SCNNode()
+    
+    override init() {
+        super.init()
+
+        let aboutMeCube = SCNBox(width: 4.0, height: 4.0, length: 4.0, chamferRadius: 1.0)
+        let aboutMeMaterial = SCNMaterial()
+        aboutMeMaterial.diffuse.contents = UIImage(named: "AboutMe")
+        aboutMeCube.materials = [aboutMeMaterial]
+
+        aboutMeNode = SCNNode(geometry: aboutMeCube)
+        aboutMeNode.name = "AboutMe"
+        aboutMeNode.rotation = SCNVector4(x: 0.0, y: 0.3, z: 0.0, w: 0.0)
+        aboutMeNode.position = SCNVector3(x: 2.0, y: 0, z: 0.0)
+        aboutMeNode.opacity = 0.0
+        self.rootNode.addChildNode(aboutMeNode)
+
+        let projectsCube = SCNBox(width: 4.0, height: 4.0, length: 4.0, chamferRadius: 1.0)
+        let projectsMaterial = SCNMaterial()
+        projectsMaterial.diffuse.contents = UIImage(named: "Projects")
+        projectsCube.materials = [projectsMaterial]
+
+        projectsNode = SCNNode(geometry: projectsCube)
+        projectsNode.name = "Projects"
+        projectsNode.rotation = SCNVector4(x: 0.0, y: 0.0, z: 0.0, w: -Float(.pi/5.0))
+        projectsNode.position = SCNVector3(x: 8.0, y: 0, z: 0.0)
+        projectsNode.opacity = 0.0
+        self.rootNode.addChildNode(projectsNode)
+        
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.rotation = SCNVector4(x: 0.3, y: 0.0, z: 0.0, w: -atan2(0.0, 0.5))
+        cameraNode.position = SCNVector3(x: 5.0, y: 0.0, z: 30.0)
+        self.rootNode.addChildNode(cameraNode)
+        
+        let light = SCNLight()
+        light.type = SCNLight.LightType.ambient
+        light.color = UIColor(white: 0.25, alpha: 0.5)
+        
+        let lightNode = SCNNode()
+        lightNode.light = light
+        self.rootNode.addChildNode(lightNode)
+        
+        self.background.contents =
+            ["BackgroundPattern.png",
+             "BackgroundPattern.png",
+             "BackgroundPattern.png",
+             "BackgroundPattern.png",
+             "BackgroundPattern.png",
+             "BackgroundPattern.png"]
+    }
+    
+    func startAnimations() {
+        
+        let aboutMeYAnimation = CABasicAnimation(keyPath: "position.y")
+        aboutMeYAnimation.fromValue = 20.0
+        aboutMeYAnimation.toValue = 0.5
+        aboutMeYAnimation.duration = 1.0
+        aboutMeYAnimation.beginTime = CACurrentMediaTime() + 0.4
+        aboutMeYAnimation.timingFunction = CAMediaTimingFunction(controlPoints: 0.5, 1.5, 1, 1)
+        aboutMeNode.addAnimation(aboutMeYAnimation, forKey: "aboutMeYAnimation")
+        
+        let aboutMeOpacityAnimation = CABasicAnimation(keyPath: "opacity")
+        aboutMeOpacityAnimation.fromValue = 0.0
+        aboutMeOpacityAnimation.toValue = 1.0
+        aboutMeOpacityAnimation.duration = 1.0
+        aboutMeOpacityAnimation.beginTime = CACurrentMediaTime() + 0.4
+        aboutMeOpacityAnimation.fillMode = CAMediaTimingFillMode.forwards
+        aboutMeOpacityAnimation.isRemovedOnCompletion = false
+        aboutMeNode.addAnimation(aboutMeOpacityAnimation, forKey: "aboutMeOpacityAnimation")
+        
+        let projectsYAnimation = CABasicAnimation(keyPath: "position.y")
+        projectsYAnimation.fromValue = -30.0
+        projectsYAnimation.toValue = -0.5
+        projectsYAnimation.duration = 1.0
+        projectsYAnimation.beginTime = CACurrentMediaTime() + 0.8
+        projectsYAnimation.timingFunction = CAMediaTimingFunction(controlPoints: 0.5, 1.5, 1, 1)
+        projectsNode.addAnimation(projectsYAnimation, forKey: "projectsYAnimation")
+        
+        let projectsOpacityAnimation = CABasicAnimation(keyPath: "opacity")
+        projectsOpacityAnimation.fromValue = 0.0
+        projectsOpacityAnimation.toValue = 1.0
+        projectsOpacityAnimation.duration = 1.0
+        projectsOpacityAnimation.beginTime = CACurrentMediaTime() + 0.8
+        projectsOpacityAnimation.fillMode = CAMediaTimingFillMode.forwards
+        projectsOpacityAnimation.isRemovedOnCompletion = false
+        projectsNode.addAnimation(projectsOpacityAnimation, forKey: "alphaAnimationRight")
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Portfolio/MainViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  Portfolio
+//
+//  Created by Andreas Lüdemann on 25/08/2019.
+//  Copyright © 2019 Andreas Lüdemann. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class MainViewController: UIViewController {
+    
+    let mainScene = MainScene()
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let sceneView = SCNView(frame: view.frame)
+        sceneView.backgroundColor = UIColor.black
+        sceneView.scene = mainScene as SCNScene
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.allowsCameraControl = true
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTouch(_:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+        view.addSubview(sceneView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        mainScene.startAnimations()
+        
+        //Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(welcome), userInfo: nil, repeats: false)
+    }
+    
+    // MARK: Methods
+    
+    /*@objc func welcome() {
+        self.presentViewController("Welcome")
+    }*/
+    
+    @objc func handleTouch(_ gestureRecognizer: UIGestureRecognizer) {
+        
+        let sceneView = SCNView(frame: view.frame)
+        let point = gestureRecognizer.location(in: sceneView)
+        
+        let hitResults = sceneView.hitTest(point, options: nil)
+        if hitResults.count > 0 {
+            
+            let result: AnyObject! = hitResults[0]
+            let material = result.node!.geometry!.firstMaterial!
+            let name = result.node!.name!
+            
+            // highlight it and present view controller
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            SCNTransaction.completionBlock = {() -> Void in
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                material.emission.contents = UIColor.black
+                SCNTransaction.commit()
+                self.presentViewController(name)
+            }
+            material.emission.contents = UIColor(white: 0.25, alpha: 0.25)
+            SCNTransaction.commit()
+        }
+        
+        view.addSubview(sceneView)
+    }
+    
+    func presentViewController(_ identifier: String) {
+        if let viewController = storyboard?.instantiateViewController(withIdentifier: identifier) {
+            present(viewController, animated: true, completion: nil)
+        }
+    }
+}
