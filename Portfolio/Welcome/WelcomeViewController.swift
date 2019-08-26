1d6e1dff97f2f620c5fe04c8c7e111dc79c72193
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-08-26 09:59:16 +0200

    Use attributed text on WelcomeViewController

--- Portfolio/Welcome/WelcomeViewController.swift
+++ Portfolio/Welcome/WelcomeViewController.swift
@@ -18,15 +18,31 @@ class WelcomeViewController: UIViewController {
     
     private let welcomeLabel: UILabel = {
        let label = UILabel()
-        label.text = """
-        Hello.
-        My name is Andreas Lüdemann and the app you're looking at is my portfolio.
-        You can swipe, pinch and drag your finger around in the following view.
-        Have fun 🎉
-        """
+        let mutableAttributedString = NSMutableAttributedString()
+        
+        let boldAttribute: [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.font: UIFont(name: "Helvetica Neue", size: 22)!,
+            NSAttributedString.Key.foregroundColor: UIColor.white
+        ]
+        let regularAttribute: [NSAttributedString.Key : Any]
+            = [
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Thin", size: 20)!,
+            NSAttributedString.Key.foregroundColor: UIColor.gray
+        ]
+        
+        let helloString = NSAttributedString(string: "Hello.\n", attributes: boldAttribute)
+        let descriptionString = NSAttributedString(string: "My name is Andreas Lüdemann and the app you're looking at is my portfolio. You can swipe, pinch and drag your finger around in the following view.", attributes: regularAttribute)
+        let haveFunString = NSAttributedString(string: "\nHave fun 🎉", attributes: boldAttribute)
+        
+        mutableAttributedString.append(helloString)
+        mutableAttributedString.append(descriptionString)
+        mutableAttributedString.append(haveFunString)
+
+        label.attributedText = mutableAttributedString
         label.numberOfLines = 0
         label.textColor = .white
         label.textAlignment = .center
+        
         return label
     }()
     
@@ -34,7 +50,7 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         
         blurEffectView.contentView.addSubview(welcomeLabel)
-        welcomeLabel.anchor(top: blurEffectView.contentView.topAnchor, leading: blurEffectView.contentView.leadingAnchor, bottom: blurEffectView.contentView.bottomAnchor, trailing: blurEffectView.contentView.trailingAnchor)
+        welcomeLabel.anchor(top: blurEffectView.contentView.topAnchor, leading: blurEffectView.contentView.leadingAnchor, bottom: blurEffectView.contentView.bottomAnchor, trailing: blurEffectView.contentView.trailingAnchor, padding: .init(top: 10, left: 10, bottom: 10, right: 10))
         
         view.addSubview(blurEffectView)
         blurEffectView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
