65f034756d43f2015d6a038df9a99c07049ea379
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-08-26 20:58:42 +0200

    Show picture in AboutMeViewController

--- Portfolio/About Me/AboutMeViewController.swift
+++ Portfolio/About Me/AboutMeViewController.swift
@@ -14,9 +14,30 @@ class AboutMeViewController: UIViewController {
     
     let myLabel: UILabel = {
         let label = UILabel()
-        label.text = "About Me"
+        label.text = "Andreas Lüdemann"
         label.textAlignment = .center
         return label
+    }()
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "me"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.borderWidth = 1.0
+        imageView.layer.borderColor = UIColor.gray.cgColor
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let roundButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 3
+        return button
     }()
     
     override func viewDidLoad() {
@@ -24,10 +45,12 @@ class AboutMeViewController: UIViewController {
         
         setupNavigationItems()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        view.addSubview(imageView)
         view.addSubview(myLabel)
         
-        myLabel.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        imageView.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 100, left: 50, bottom: 0, right: 50))
+        myLabel.anchor(top: imageView.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 15, left: 0, bottom: 0, right: 0))
     }
     
     private func setupNavigationItems() {
